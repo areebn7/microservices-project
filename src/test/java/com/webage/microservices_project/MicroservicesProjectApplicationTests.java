fc49d177e4e9d567ffd25c691f8202709311a7e2
Areeb Nabi <anabi001@odu.edu>
2024-08-19 12:29:54 -0400

    first commit

---
+++ src/test/java/com/webage/microservices_project/MicroservicesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webage.microservices_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
