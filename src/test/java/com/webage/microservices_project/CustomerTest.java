bdc85e854951bca675f5f75ba04c7ab3f487fa18
Areeb Nabi <anabi001@odu.edu>
2024-08-20 10:51:08 -0400

    Create unit tests for customer

--- src/test/java/com/webage/microservices_project/CustomerTest.java
+++ src/test/java/com/webage/microservices_project/CustomerTest.java
@@ -1,7 +1,13 @@
 package com.webage.microservices_project;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.webage.microservices_project.API.Model.Customer;
+
+
 
 @SpringBootTest
 class CustomerTest {
@@ -10,4 +16,55 @@ class CustomerTest {
 	void contextLoads() {
 	}
 
+    @Test
+    public void testConstructor() {
+        // Create a customer object using the parameterized constructor
+        Customer customer = new Customer(1, "Areeb Nabi", "areeb123", "areeb.nabi@gmail.com");
+
+        // Verify that the fields are initialized correctly
+        assertEquals(1, customer.getId());
+        assertEquals("Areeb Nabi", customer.getName());
+        assertEquals("areeb123", customer.getPassword());
+        assertEquals("areeb.nabi@gmail.com", customer.getEmail());
+    }
+
+    @Test
+    public void testSetAndGetId() {
+        Customer customer = new Customer();
+        
+        int id = 1;
+        customer.setId(id);
+
+        assertEquals(id, customer.getId());
+    }
+
+    @Test 
+    public void testSetAndGetName() {
+        Customer customer = new Customer();
+
+        String name = "Areeb";
+        customer.setName(name);
+
+        assertEquals(name, customer.getName());
+    }
+
+    @Test
+    public void testSetAndGetPassword() {
+        Customer customer = new Customer();
+
+        String password = "password321";
+        customer.setPassword(password);
+
+        assertEquals(password, customer.getPassword());
+    }
+
+    @Test
+    public void testSetAndGetEmail() {
+        Customer customer = new Customer();
+
+        String email = "areeb.nabi@blah.com";
+        customer.setEmail(email);
+
+        assertEquals(email, customer.getEmail());
+    }
 }
