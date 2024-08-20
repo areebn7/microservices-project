0a14a7048169ff77ab69089a5bb922eb6a400e9d
Areeb Nabi <anabi001@odu.edu>
2024-08-20 14:50:27 -0400

    Add database integration

--- src/main/java/com/webage/microservices_project/API/Controller/CustomerController.java
+++ src/main/java/com/webage/microservices_project/API/Controller/CustomerController.java
@@ -1,47 +1,84 @@
 package com.webage.microservices_project.API.Controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.webage.microservices_project.API.Model.Customer;
+import com.webage.microservices_project.API.repository.CustomersRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/api")
 public class CustomerController {
 
-    @GetMapping("/api")
+    @Autowired CustomersRepository repo;
+
+    @GetMapping
     public String homePage() {
         return "Welcome to my REST API!";
     }
 
-    private List<Customer> customers = new ArrayList<>();
 
-    public CustomerController() {
-        customers.add(new Customer(1, "Areeb Nabi", "password123", "areeb.nabi@gmail.com"));
-        customers.add(new Customer(2, "John Doe", "johndoe456", "john.doe@example.com"));
-        customers.add(new Customer(3, "Jane Smith", "janesmith789", "jane.smith@example.com"));
-    }
-
-    // Get all customers
     @GetMapping("/customers")
-    public List<Customer> getAllCustomers() {
-        return customers;
+    public Iterable<Customer> getAll() {
+        return repo.findAll();
     }
-        // Get a customer by ID
-    @GetMapping("/customers/{id}")
-    public Customer getCustomerById(@PathVariable("id") int id) {
-        Optional<Customer> customer = customers.stream()
-                .filter(c -> c.getId() == id)
-                .findFirst();
-        return customer.orElse(null);
+
+    @GetMapping("/customers/{id}") 
+    public Optional<Customer> getCustomer(@PathVariable("id")int id) {
+        return repo.findById(id);
+        
+    }
+
+    
+    @PostMapping("/customers")
+    public ResponseEntity<?> addCustomer(@RequestBody Customer newCustomer) {
+        // Validate input:
+        if ( newCustomer.getName()==null
+            || newCustomer.getEmail() == null) { 
+            return ResponseEntity.badRequest().build();
+        }
+        newCustomer=repo.save(newCustomer);
+
+        // Location header will resemble 
+        // "http://localhost:8080/customers/27"
+        URI location =
+            ServletUriComponentsBuilder
+            .fromCurrentRequest()
+            .path("/{id}")
+            .buildAndExpand(newCustomer.getId())
+            .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+     @PutMapping("/{id}")
+    public ResponseEntity<?> putCustomer(
+        @RequestBody Customer customer, 
+        @PathVariable long id) {
+
+        if (customer.getId()!=id
+            || customer.getName()==null
+            || customer.getEmail() == null) {
+                return ResponseEntity.badRequest().build();
+        }
+
+        repo.save(customer);
+        return ResponseEntity.ok().build();
     }
 }
